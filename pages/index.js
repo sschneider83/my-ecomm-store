090a7ab33d00042622e805aa9986952e924b52aa
Stephan Schneider <stephanschneider@gmail.com>
2021-05-21 14:14:32 +0200

    updateing the description

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          The best place to buy cryptocurrency
+          The best place in the universe to buy cryptocurrency
         </p>
 
         <ul className={styles.grid}>
